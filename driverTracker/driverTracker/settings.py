8c0c2b7e50b9f3adb0f066a417df0af62674774e
gift <ms.thunradee@gmail.com>
2021-11-29 00:33:51 -0800

    remove secret key

--- driverTracker/driverTracker/settings.py
+++ driverTracker/driverTracker/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-521d9x&s^n2dx8k3!0my6m*#6+7$(1w%n)ivp8!msz#d)u!b#f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
